0988864d49ab6286d1e0d48fae3e96c1edf34d0f
langfjord <16120146+langfjord@users.noreply.github.com>
2019-06-20 10:01:32 +0200

    Create wordpress-plugin-starter.php

---
+++ wordpress-plugin-starter.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Plugin Name: Wordpress Plugin Starter
+* Description: This plugin contains all of the custom functions.
+* Author: Langfjord
+* Version: 0.1
+*/
+
+/* PLUGIN CODE START */
+
+/* PLUGIN CODE END */
+
+?>
